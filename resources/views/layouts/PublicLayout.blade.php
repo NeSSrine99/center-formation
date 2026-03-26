81c5c0ae2161abd75db045ed2286a047f8a8004f
nesrin <103462967+Mnasrinesrine@users.noreply.github.com>
2026-03-26 21:41:11 +0100

    nesrine

--- resources/views/layouts/PublicLayout.blade.php
+++ resources/views/layouts/PublicLayout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Center Des Formations') }}</title>
 
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.ico') }}" rel="icon">
